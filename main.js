13426f54b3f5f3635d0c4aee548ed93bfabc3b18
SangKwon <sangkwon@innofin.co.kr>
2022-02-21 15:59:48 +0900

    정규식 공부

---
+++ main.js
@@ -0,0 +1,42 @@
+let str = `
+010-1234-5678
+thesecon@gmail.com
+https://www.ombapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`;
+
+// const regexp = /fox/gi;
+// console.log(regexp.test(str));
+// console.log(str.match(regexp));
+// console.log(str.replace(regexp, "AAA"));
+
+// const regexp = /the/gi;
+// console.log(str.match(regexp));
+
+// console.log(str.match(/http/gim));
+// console.log(str.match(/https?/g));
+// console.log(str.match(/h..p/gim));
+// console.log(str.match(/fox|dog/gim));
+
+// console.log(str.match(/d{2}/gim));
+// console.log(str.match(/d{2,3}/gim));
+// console.log(str.match(/\b\w{2,3}\b/g));
+
+// console.log(str.match(/[0-9]/g));
+// console.log(str.match(/[0-9]{1,/g));
+// console.log(str.match(/[가-힣]{1,/g));
+
+// console.log(str.match(/\w/g));
+// console.log(str.match(/\bf\w{1,0}\b/g));
+// console.log(str.match(/\d{1,}/g));
+// console.log(str.match(/\s/g));
+
+// const h = `   the  hello world
+
+// `;
+
+// console.log(h.replace(/\s/g, ""));
+
+// console.log(str.match(/.{1,}(?=@))/g);
+// console.log(str.match(/(?<=@)).{1.}/g);
